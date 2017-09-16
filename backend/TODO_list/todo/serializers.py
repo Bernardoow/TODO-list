10dbe86c456d7dee8f49b8959bdae45161b0536a
Bernardo Gomes <bgomesdeabreu@gmail.com>
2017-09-15 21:08:08 -0300

    Added django-rest and created serializer to model task

---
+++ backend/TODO_list/todo/serializers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from rest_framework import serializers
+
+from todo.models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('title', 'description', 'open_date',
+                  'completed_date', 'isRemoved', 'positionOrder')
